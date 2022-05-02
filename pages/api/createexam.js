bbb8491ddb4a07398539a5ad9055e21c49dbc201
Lycanfloof <lyca2206@gmail.com>
2022-05-02 06:01:12 -0500

    Now you cannot create exams with the same access code

--- pages/api/createexam.js
+++ pages/api/createexam.js
@@ -5,13 +5,18 @@ export default async function handler(req, res) {
     const exam = req.body;
     if (method === "POST") {
         try {
-            testdatabase.push({
-                examName: exam.examName,
-                accessCode: exam.accessCode,
-                questions: exam.questions,
-                correctAnswers: exam.correctAnswers
-            });
-            res.status(200).send({ response: true });
+            let wasCreated = false;
+            const testIndex = testdatabase.findIndex(element => element.accessCode == exam.accessCode);
+            if (testIndex == -1 && exam.questions.length > 0) {
+                wasCreated = true;
+                testdatabase.push({
+                    examName: exam.examName,
+                    accessCode: exam.accessCode,
+                    questions: exam.questions,
+                    correctAnswers: exam.correctAnswers
+                });
+            }
+            res.status(200).send({ response: wasCreated });
         } catch (error) {
             res.status(500).send({ response: "Oops, something went wrong!" })
         }
