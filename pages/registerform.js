d28a495d4087d5f9ca3a2370441056e4adf7d957
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-05-01 23:38:52 -0500

    Style applied to registerform

--- pages/registerform.js
+++ pages/registerform.js
@@ -1,4 +1,8 @@
-import Router from 'next/router'
+import Router from 'next/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 let state = {
     username: "",
@@ -8,23 +12,36 @@ let state = {
 
 export default function register() {
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Username:</label>
-                <input required placeholder="username" type="text" name="username" onChange={handleInputChange}></input>
-                <br />
-                <label>Password:</label>
-                <input required placeholder="password" type="password" name="password" onChange={handleInputChange}></input>
-                <br />
-                <label>Account Type:</label>
-                <select required name="account-type" onChange={handleInputChange}>
-                    <option value="v1">Teacher</option>
-                    <option value="v2">Student</option>
-                </select>
-                <br />
-                <button type="submit">Sign in!</button>
-            </form>
-            <button onClick={redirectToLogInForm}>Log in!</button>
+        <div class="center positioned">
+          <Card style={{ width: '30rem' }}>
+            <Card.Body>
+            <Card.Title class="padd">Nice to meet you! </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Join us</Card.Subtitle>
+            <br/>
+
+              <Form onSubmit={handleSubmit}>
+                <Form.Label>Username</Form.Label>
+                <Form.Control required placeholder="username" type="text" name="username" onChange={handleInputChange}></Form.Control>
+                <br/>
+                <Form.Label>Password</Form.Label>
+                <Form.Control required placeholder="password" type="password" name="password" onChange={handleInputChange}></Form.Control>
+                <br/>
+                <Form.Label>Account type</Form.Label>
+                <Form.Select aria-label="Default select example" required name="account-type" onChange={handleInputChange}>
+                  <option value="1">Teacher</option>
+                  <option value="2">Student</option>
+                </Form.Select>
+                <br/>
+                <div className="d-grid gap-2">
+                  <Button variant="dark" size="lg" type="submit">Sign in!</Button>
+                </div>
+              </Form>
+              <br/>
+              <div className="d-grid gap-2">
+                <Button variant="dark" size="lg" onClick={redirectToLogInForm}>Log in!</Button>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
     )
 }
