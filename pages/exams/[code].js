52792305854694e01f3c3a24a700a53e54cd3b8a
Julian Bolanos <julian.camilob24@gmail.com>
2022-05-01 21:38:34 -0500

    Front of showing exams added

--- pages/exams/[code].js
+++ pages/exams/[code].js
@@ -1,32 +1,55 @@
+import Router from 'next/router'
 export default function Exam({examData}) {
-  
+  examData = examData.response;
   let exam = (
-  <div>
-    {examData.examName}
-    <br/>
-    {examData.accessCode}
-    <br/>
-  </div>);
 
-    for (const question in examData.questions) {
-      exam +=
-      <div>
+    <div>
+      {examData.examName}
+      <br/>
+      {examData.accessCode}
+      <br/>
+    </div>);
+
+    let array = [];
+
+    for (let i = 0; i < examData.questions.length; i++) {
+      let question = examData.questions[i];
+      let aux =(
+        <div>
         <p>{question.statement}</p>
         <form>
           <input type="radio"/>
-          <label for="answerA">{question.options.get(0)}</label>
+          <label for="answerA">{question.options[0]}</label>
           <br/>
           <input type="radio"/>
-          <label for="answerB">{question.options.get(1)}</label>
+          <label for="answerB">{question.options[1]}</label>
           <br/>
           <input type="radio"/>
-          <label for="answerC">{question.options.get(2)}</label>
+          <label for="answerC">{question.options[2]}</label>
           <br/>
           <input type="radio"/>
-          <label for="answerD">{question.options.get(3)}</label>
+          <label for="answerD">{question.options[3]}</label>
         </form>
       </div>
+      )
+
+      array[i] = aux;
     }
 
-  return exam
+  return (
+    <div>
+      {examData.examName}
+      <br/>
+      {examData.accessCode}
+      <br/>
+      {array}
+    </div>
+    )
+}
+
+Exam.getInitialProps = async () => {
+  //const path = Router.pathname.split();
+  const exam = await fetch("http://localhost:3000/api/obtaintest/AAAA");
+  const result = await exam.json();
+  return { examData: result }
 }
