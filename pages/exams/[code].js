50fba6c9d654c92f374303954d2efa6c9f44ceec
Lycanfloof <lyca2206@gmail.com>
2022-05-01 22:08:52 -0500

    Fixed errors in [code].js

--- pages/exams/[code].js
+++ pages/exams/[code].js
@@ -1,55 +1,45 @@
 import Router from 'next/router'
-export default function Exam({examData}) {
-  examData = examData.response;
-  let exam = (
+export default function Exam({ examData }) {
+  let array = [];
 
-    <div>
-      {examData.examName}
-      <br/>
-      {examData.accessCode}
-      <br/>
-    </div>);
-
-    let array = [];
-
-    for (let i = 0; i < examData.questions.length; i++) {
-      let question = examData.questions[i];
-      let aux =(
-        <div>
-        <p>{question.statement}</p>
+  for (let i = 0; i < examData.questions.length; i++) {
+    let question = examData.questions[i];
+    let aux = (
+      <div key={examData.accessCode + i}>
+        <p>{String.fromCharCode(i+65) + "." + " " + question.statement}</p>
         <form>
-          <input type="radio"/>
-          <label for="answerA">{question.options[0]}</label>
-          <br/>
-          <input type="radio"/>
-          <label for="answerB">{question.options[1]}</label>
-          <br/>
-          <input type="radio"/>
-          <label for="answerC">{question.options[2]}</label>
-          <br/>
-          <input type="radio"/>
-          <label for="answerD">{question.options[3]}</label>
+          <input type="radio" />
+          <label name="answerA">{question.options[0]}</label>
+          <br />
+          <input type="radio" />
+          <label name="answerB">{question.options[1]}</label>
+          <br />
+          <input type="radio" />
+          <label name="answerC">{question.options[2]}</label>
+          <br />
+          <input type="radio" />
+          <label name="answerD">{question.options[3]}</label>
         </form>
       </div>
-      )
+    )
 
-      array[i] = aux;
-    }
+    array[i] = aux;
+  }
 
   return (
     <div>
-      {examData.examName}
-      <br/>
-      {examData.accessCode}
-      <br/>
+      Name: {examData.examName}
+      <br />
+      Code: {examData.accessCode}
+      <br />
       {array}
     </div>
-    )
+  )
 }
 
 Exam.getInitialProps = async () => {
-  //const path = Router.pathname.split();
+  //const path = Router.pathname;
   const exam = await fetch("http://localhost:3000/api/obtaintest/AAAA");
   const result = await exam.json();
-  return { examData: result }
+  return { examData: result.response }
 }
