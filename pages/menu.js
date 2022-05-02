0552388225a90e1bac9a49d31b9ce7204de45f7d
Lycanfloof <lyca2206@gmail.com>
2022-05-01 22:22:38 -0500

    Now we import the testdatabase with backend (yes, again)

--- pages/menu.js
+++ pages/menu.js
@@ -1,22 +1,20 @@
 import Router from 'next/router'
 import React from 'react'
-import testdatabase from '../data/testdatabase'
 
-export default function menu() {
+export default function menu({ testdatabase }) {
     return (
         <div>
             <label>
                 Exam list:
             </label>
             <div id="exam-list">
-                <ShowExamList />
+                <ShowExamList testdatabase={testdatabase.response} />
             </div>
         </div>
     )
 }
 
 menu.getInitialProps = async () => {
-    //It would be preferable to find a way to put the relative path in this fetch().
     const database = await fetch("http://localhost:3000/api/obtaintestdatabase");
     const result = await database.json();
     return { testdatabase: result }
@@ -24,11 +22,15 @@ menu.getInitialProps = async () => {
 
 class ShowExamList extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     showExam(code) {
-        Router.push("http://localhost:3000/exams/" + code);
     }
 
     getExams() {
+        let testdatabase = this.props.testdatabase;
         let examList = [];
         for (let i = 0; i < testdatabase.length; i++) {
             examList.push(
