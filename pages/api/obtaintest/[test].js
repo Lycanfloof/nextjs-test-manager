51ce61568fc207b367116dc01ccb8ff6bc4f10d6
Lycanfloof <lyca2206@gmail.com>
2022-05-01 20:24:23 -0500

    Fixed path error in obtaintest and made it work with accessCode instead

--- pages/api/obtaintest/[test].js
+++ pages/api/obtaintest/[test].js
@@ -1,9 +1,9 @@
-import testdatabase from '../../data/testdatabase'
+import testdatabase from '../../../data/testdatabase'
 
 export default async function handler(req, res) {
         try {
-            let testName = req.query.test;
-            let test = testdatabase.find(element => element.examName == testName);
+            let testCode = req.query.test;
+            let test = testdatabase.find(element => element.accessCode == testCode);
             res.status(200).send({ response: test });
         } catch (error) {
             res.status(500).send({ response: "Oops, something went wrong!" })
